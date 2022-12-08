22b347a36ce3f48ad693c72df09567cf421f4e47
Sergey Kravcov <sergey.kravcov.job@gmail.com>
2022-12-08 23:54:14 +0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
+import 'material-icons/iconfont/material-icons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
